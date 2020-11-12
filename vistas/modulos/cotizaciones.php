8cf279f9a2567c707cd7627f49927fa61fcabc21
alejandro perez <alex@connecmedia.com>
2020-11-12 13:19:38 -0600

    proyecto inicial

---
+++ vistas/modulos/cotizaciones.php
@@ -0,0 +1,63 @@
+<div class="content-wrapper">
+
+  <section class="content-header">
+
+    <h1>Control de cotizaciones</h1>
+
+    <ol class="breadcrumb">
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      <li class="active">Control de cotizaciones</li>
+    </ol>
+  </section>
+
+  <section class="content">
+
+    <div class="box">
+      <div class="box-header with-border">
+        <a href="crear-venta">
+          <button class="btn btn-primary">Crear cotización</button>
+        </a>
+      </div>
+
+      <div class="box-body">
+        
+       <table class="table table-bordered table-striped dt-responsive tablas">
+        <thead>
+         <tr>
+           <th style="width:10px">#</th>
+           <th>No. de cotización</th>
+           <th>Cliente</th>
+           <th>Empresa</th>
+           <th>Vendedor</th>
+           <th>estado</th>
+           <th>Sub total</th>
+           <th>Iva</thIva>
+           <th>Total</th> 
+           <th>Fecha</th>
+           <th>Acciones</th>
+         </tr> 
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>1000123</td>
+            <td>Juan Villegas</td>
+            <td>Julio Gómez</td>
+            <td>TC-12412425346</td>
+            <td>$ 1,000.00</td>
+            <td>$ 1,190.00</td>
+            <td>2017-12-11 12:05:32</td>
+            <td>
+              <div class="btn-group">
+                <button class="btn btn-info"><i class="fa fa-print"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+              </div>  
+            </td>
+          </tr>
+        </tbody>
+       </table>
+      </div>
+    </div>
+  </section>
+</div>
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2020 <a href="http://connecmedia.com/" target="_blank">Connecmedia </a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
